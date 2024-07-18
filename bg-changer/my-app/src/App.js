3d84d0c0629a508a20cafcca471fac3dce7ace98
Abdul Basit Sher <abasitsher1@gmail.com>
2024-07-19 00:36:31 +0500

    ok all set

--- bg-changer/my-app/src/App.js
+++ bg-changer/my-app/src/App.js
@@ -1,19 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState();
+  
   return (
-    <div className="App">
-      <button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
-
-<button type="button" class="btn btn-link">Link</button>
+    <div className="App position-center" style={{backgroundColor: color , alignItems: "center", width: "100%",height:"800px"}}>
+      <div className= " rounded-2" style={{backgroundColor: color, width: "70%", position: 'fixed', bottom: '50px', display: 'flex', justifyContent: 'space-around' }}>
+        <button onClick={() => setColor("red")} 
+          type="button" className="btn btn-primary m-0.1" style={{ width: '10%' }}>Primary</button>
+        <button onClick={() => setColor("gray")} 
+          type="button" className="btn btn-secondary m-0.1" style={{ width: '10%' }}>Secondary</button>
+        <button onClick={() => setColor("green")} 
+          type="button" className="btn btn-success m-0.1" style={{ width: '10%' }}>Success</button>
+        <button onClick={() => setColor("red")} 
+          type="button" className="btn btn-danger m-0.1" style={{ width: '10%' }}>Danger</button>
+        <button onClick={() => setColor("yellow")} 
+          type="button" className="btn btn-warning m-0.1" style={{ width: '10%' }}>Warning</button>
+        <button onClick={() => setColor("cyan")} 
+          type="button" className="btn btn-info m-0.1" style={{ width: '10%' }}>Info</button>
+        <button onClick={() => setColor("white")} 
+          type="button" className="btn btn-light m-0.1" style={{ width: '10%' }}>Light</button>
+        <button onClick={() => setColor("black")} 
+          type="button" className="btn btn-dark m-0.1" style={{ width: '10%' }}>Dark</button>
+      </div>
+      
     </div>
   );
 }
