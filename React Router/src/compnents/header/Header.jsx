36b45167d8f5b08141c012a1475940a7246152ce
Abdul basit sher <abasitsher1@gmail.com>
2025-01-03 19:28:26 +0500

    react router is done too

--- React Router/src/compnents/header/Header.jsx
+++ React Router/src/compnents/header/Header.jsx
@@ -42,6 +42,7 @@ export default function Header() {
                                 </NavLink>  </li>
                                 <li>
                                  <NavLink
+                                 to='/About'
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${( isActive ? "text-orange-500" : "text-black-300")} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
